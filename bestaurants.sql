2a576ee687fb7f005fdfbf1a7c74add2533aaef1
Nathan Stewart and Steve Spitz <nathan@>
2017-07-13 15:37:26 -0700

    add updated sql file

--- bestaurants.sql
+++ bestaurants.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Jul 13, 2017 at 12:07 AM
+-- Generation Time: Jul 14, 2017 at 12:36 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -32,7 +32,15 @@ CREATE TABLE `cuisine` (
   `name` varchar(255) DEFAULT NULL,
   `restaurant_id` int(11) DEFAULT NULL,
   `id` bigint(20) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cuisine`
+--
+
+INSERT INTO `cuisine` (`name`, `restaurant_id`, `id`) VALUES
+('Haitian', NULL, 25),
+('Nepalese', NULL, 26);
 
 -- --------------------------------------------------------
 
@@ -43,8 +51,42 @@ CREATE TABLE `cuisine` (
 CREATE TABLE `restaurants` (
   `name` varchar(255) DEFAULT NULL,
   `cuisine_id` int(11) DEFAULT NULL,
-  `id` bigint(20) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  `id` bigint(20) unsigned NOT NULL,
+  `description` text
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `restaurants`
+--
+
+INSERT INTO `restaurants` (`name`, `cuisine_id`, `id`, `description`) VALUES
+('Jeo', 25, 20, 'Hey Mon'),
+('Bob', 25, 21, 'solid'),
+('Bhuddas', 26, 22, 'nirvana');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` bigint(20) unsigned NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `stars` int(11) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `restaurant_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `title`, `stars`, `content`, `restaurant_id`) VALUES
+(1, 'fuckall of this', 3, 'laiuyrgo9', 18),
+(2, 'Good Stuff!', 5, 'that chickin is hawt!', 20),
+(3, 'Transcendent', 5, 'I feel enlightened', 22),
+(4, 'Terribad', 1, 'i think i just puked in my mouth', 20);
 
 --
 -- Indexes for dumped tables
@@ -65,6 +107,13 @@ ALTER TABLE `restaurants`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -72,12 +121,17 @@ ALTER TABLE `restaurants`
 -- AUTO_INCREMENT for table `cuisine`
 --
 ALTER TABLE `cuisine`
-  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `restaurants`
 --
 ALTER TABLE `restaurants`
-  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
